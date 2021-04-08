d10cfef3ee5651fe4f08df3ef028b3b0e6a584fc
jose Andres <jcastanedaforno@gmail.com>
2021-04-07 21:38:02 -0600

    event loop node js

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const _ = require('lodash');
 
-const items = [1, [2,[3, [4]]]]
+const {readFile} = require('fs');
 
-const newItems = _.flatMapDeep(items);
-console.log(items);
+console.log('started a first task');
+// Check file path 
+readFile('./content/first.txt','utf8', ((err, result) => {
+  if(err){
+    console.log(err);
+    return;
+  } 
+  console.log(result);
+  console.log('Conpleted first task');
+}));
+// Set timeout
+setTimeout(()=>{
+  console.log('time out task finished');
+},0);
+// Set interval - the vent loop will invoque the function every 2 sec.
+setInterval(()=>{
+  console.log('Hello word!');
+},200);
+
+console.log('starting next task');
